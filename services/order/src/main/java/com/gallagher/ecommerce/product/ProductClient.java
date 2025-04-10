cf19f27c90cbe0d64da38d7661812c56857f480a
Franck Judes <93270724+FranckJudes@users.noreply.github.com>
2025-04-10 18:17:35 +0100

    11-04-2025

--- services/order/src/main/java/com/gallagher/ecommerce/product/ProductClient.java
+++ services/order/src/main/java/com/gallagher/ecommerce/product/ProductClient.java
@@ -2,7 +2,6 @@ package com.gallagher.ecommerce.product;
 
 import com.gallagher.ecommerce.exceptions.BusinessException;
 import com.gallagher.ecommerce.orders.PurchaseRequest;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -12,8 +11,8 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-@Service
 @RequiredArgsConstructor
+@Service
 public class ProductClient {
 
     @Value("${application.config.product-url}")
@@ -21,21 +20,22 @@ public class ProductClient {
     private final RestTemplate restTemplate;
 
     public List<PurchaseResponse> purchaseProducts(List<PurchaseRequest> requestBody){
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
-        HttpEntity<List<PurchaseRequest>> requestEntity = new HttpEntity<>(requestBody, headers);
-        ParameterizedTypeReference<List<PurchaseResponse>> responseType =
-                new ParameterizedTypeReference<List<PurchaseResponse>>() {};
-        ResponseEntity<List<PurchaseResponse>> responseEntity = restTemplate.exchange(
-                productUrl + "/purchases",
-                HttpMethod.POST,
-                requestEntity,
-                responseType);
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
-        if (responseEntity.getStatusCode().isError()) {
-            throw new BusinessException("An error occurred while processiong the products purchases :" + responseEntity.getStatusCode());
-        }
-        return responseEntity.getBody();
+            HttpEntity<List<PurchaseRequest>> requestEntity = new HttpEntity<>(requestBody, headers);
+            ParameterizedTypeReference<List<PurchaseResponse>> responseType =
+                    new ParameterizedTypeReference<List<PurchaseResponse>>() {};
+            ResponseEntity<List<PurchaseResponse>> responseEntity = restTemplate.exchange(
+                    productUrl + "/purchases",
+                    HttpMethod.POST,
+                    requestEntity,
+                    responseType);
+
+            if (responseEntity.getStatusCode().isError()) {
+                throw new BusinessException("An error occurred while processiong the products purchases :" + responseEntity.getStatusCode());
+            }
+            return responseEntity.getBody();
     }
 }
